da5dc1c160c12ec6cfb7d2a0781a2177f4e08685
AsuraMoon <jufredenucci@gmx.fr>
2021-11-09 12:59:32 +0100

    Initial commit

---
+++ Fruits.js
@@ -0,0 +1,27 @@
+var tablefruits = []
+
+tablefruits[0]= "Orange";
+tablefruits[1]= "Pêche";
+tablefruits[2]= "Fraise";
+tablefruits[3]= "Framboise";
+
+console.log(tablefruits);
+
+var Orange = {
+    color : "Orange",
+    size : "Moyen +"
+}
+var Pêche = {
+    color : "Rose Pale",
+    size : "Petit +"
+}
+var Fraise = {
+    color : "Rouge",
+    size : "Petit"
+}
+var Framboise = {
+    color : "Violet",
+    size : "Petit -"
+}
+
+console.log(Orange, Pêche, Fraise, Framboise)
